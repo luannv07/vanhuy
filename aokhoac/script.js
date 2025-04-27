6af761c1d3abdbaddcec16b52843bd8c20dab7d0
luan <vanluandvlp@gmail.com>
2025-04-27 19:46:37 +0700

    trang 2

---
+++ aokhoac/script.js
@@ -0,0 +1,295 @@
+const data = [
+  {
+    "name": "Áo khoác dạ tweed viền phối đính cúc sang chảnh",
+    "price": 630000,
+    "color": "Kẻ zigzag",
+    "size": "L",
+    "sold": 102,
+    "date": "12/06/2024",
+    "img": "images/aokhoac23.jpeg"
+  },
+  {
+    "name": "Áo khoác dạ dáng ngắn cổ tròn phối cổ và tay",
+    "price": 899000,
+    "color": "Xanh dương pastel",
+    "size": "M",
+    "sold": 205,
+    "date": "17/04/2024",
+    "img": "images/aokhoac1.jpeg"
+  },
+  {
+    "name": "Áo khoác dạ tweed dáng lửng cổ rời",
+    "price": 799000,
+    "color": "Cốm",
+    "size": "L",
+    "sold": 135,
+    "date": "20/04/2024",
+    "img": "images/aokhoac8.jpeg"
+  },
+  {
+    "name": "Áo khoác dạ tweed phối lật gấu tay",
+    "price": 799000,
+    "color": "Đen kim tuyến",
+    "size": "S",
+    "sold": 198,
+    "date": "22/04/2024",
+    "img": "images/aokhoac3.jpeg"
+  },
+  {
+    "name": "Áo khoác dạ cổ tròn túi cơi",
+    "price": 899000,
+    "color": "Kẻ zigzag",
+    "size": "M",
+    "sold": 142,
+    "date": "25/04/2024",
+    "img": "images/aokhoac4.jpeg"
+  },
+  {
+    "name": "Áo khoác dạ tweed viền dây dạ",
+    "price": 799000,
+    "color": "Xanh",
+    "size": "L",
+    "sold": 123,
+    "date": "27/04/2024",
+    "img": "images/aokhoac5.jpeg"
+  },
+  {
+    "name": "Áo khoác dạ tweed dáng rút rua vạt tròn",
+    "price": 779000,
+    "color": "Tím",
+    "size": "S",
+    "sold": 156,
+    "date": "02/05/2024",
+    "img": "images/aokhoac7.jpeg"
+  },
+  {
+    "name": "Áo cách điệu cổ thủy thủy phối ren",
+    "price": 449000,
+    "color": "Xanh lá pastel",
+    "size": "L",
+    "sold": 129,
+    "date": "05/05/2024",
+    "img": "images/aokhoac8.jpeg"
+  },
+  {
+    "name": "Áo blazer phối viền trắng chân",
+    "price": 629000,
+    "color": "Rêu",
+    "size": "S",
+    "sold": 204,
+    "date": "07/05/2024",
+    "img": "images/aokhoac9.jpeg"
+  },
+  {
+    "name": "Áo blazer cổ V tay cộc",
+    "price": 619000,
+    "color": "Xanh than",
+    "size": "M",
+    "sold": 188,
+    "date": "10/05/2024",
+    "img": "images/aokhoac10.jpeg"
+  },
+  {
+    "name": "Áo khoác dạ tweed dáng suông cài cúc",
+    "price": 549000,
+    "color": "Ghi",
+    "size": "L",
+    "sold": 116,
+    "date": "12/05/2024",
+    "img": "images/aokhoac11.jpeg"
+  },
+  {
+    "name": "Áo khoác dáng lửng túi ốp",
+    "price": 580000,
+    "color": "Be nhạt",
+    "size": "S",
+    "sold": 140,
+    "date": "15/05/2024",
+    "img": "images/aokhoac12.jpeg"
+  },
+  {
+    "name": "Áo blazer dáng ôm chiết eo",
+    "price": 840000,
+    "color": "Đen",
+    "size": "M",
+    "sold": 167,
+    "date": "17/05/2024",
+    "img": "images/aokhoac13.jpeg"
+  },
+  {
+    "name": "Áo khoác dáng lửng phối cổ",
+    "price": 690000,
+    "color": "Xanh lá",
+    "size": "L",
+    "sold": 134,
+    "date": "20/05/2024",
+    "img": "images/aokhoac14.jpeg"
+  },
+  {
+    "name": "Áo khoác dạ dáng lửng phối cổ ren",
+    "price": 680000,
+    "color": "Hồng",
+    "size": "S",
+    "sold": 122,
+    "date": "22/05/2024",
+    "img": "images/aokhoac15.jpeg"
+  },
+  {
+    "name": "Áo blazer dáng ôm chiết ly eo",
+    "price": 510000,
+    "color": "Vàng",
+    "size": "M",
+    "sold": 175,
+    "date": "25/05/2024",
+    "img": "images/aokhoac16.jpeg"
+  },
+  {
+    "name": "Áo blazer kẻ thanh lịch",
+    "price": 730000,
+    "color": "Kem",
+    "size": "L",
+    "sold": 143,
+    "date": "27/05/2024",
+    "img": "images/aokhoac17.jpeg"
+  },
+  {
+    "name": "Áo gile dạ tweed trẻ trung",
+    "price": 420000,
+    "color": "Đỏ",
+    "size": "S",
+    "sold": 161,
+    "date": "30/05/2024",
+    "img": "images/aokhoac18.jpeg"
+  },
+  {
+    "name": "Áo blazer cổ cách điệu phối viền sang chảnh",
+    "price": 650000,
+    "color": "Nâu",
+    "size": "M",
+    "sold": 148,
+    "date": "02/06/2024",
+    "img": "images/aokhoac19.jpeg"
+  },
+  {
+    "name": "Áo trenchcoat dáng dài thanh lịch",
+    "price": 980000,
+    "color": "Be",
+    "size": "L",
+    "sold": 112,
+    "date": "05/06/2024",
+    "img": "images/aokhoac20.jpeg"
+  },
+  {
+    "name": "Áo blazer cổ tròn phối dây tweed sang chảnh",
+    "price": 530000,
+    "color": "Trắng",
+    "size": "S",
+    "sold": 230,
+    "date": "07/06/2024",
+    "img": "images/aokhoac21.jpeg"
+  },
+  {
+    "name": "Áo blazer dáng ngắn trẻ trung",
+    "price": 560000,
+    "color": "Kẻ trắng",
+    "size": "M",
+    "sold": 180,
+    "date": "10/06/2024",
+    "img": "images/aokhoac22.jpeg"
+  }
+]
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderProduct();
+  collapseFilterButton();
+})
+const renderProduct = () => {
+  const ulProduct = document.querySelector(".content-product-mid ul");
+  ulProduct.innerHTML = "";
+  data.forEach(item => {
+    ulProduct.innerHTML += `
+       <li>
+              <div class="">
+                <div class="spoiler-img">
+                  <a href="#" class="product-preview" style="display: block;overflow: hidden;">
+                    <img src="${item.img}" class="product-preview-img">
+                  </a>
+                  <div class="sub">
+                    <a href="#">Xem chi tiết</a>
+                    <a href="#">Mua ngay</a>
+                  </div>
+                </div>
+                <div class="spoiler-details">
+                  <div class="item-name">
+                    <a href="#" class="tp_product_name overflow-text" style="display: block;">
+                      <span>${item.name}</span>
+                    </a>
+                  </div>
+                  <div class="item-rate">
+                    <i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path
+                          d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                      </svg></i>
+                    <i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path
+                          d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                      </svg></i>
+                    <i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path
+                          d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                      </svg></i>
+                    <i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path
+                          d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                      </svg></i>
+                    <i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path
+                          d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                      </svg></i>
+                    <span>(${item.sold} đã bán)</span>
+                  </div>
+
+                  <div class="item-prices">
+                    <span>${formatDong(item.price)}<span>₫</span></span>
+                  </div>
+                  <div class="item-btn mrg-top-5">
+                    <button class="item-addbtn">Thêm vào giỏ hàng</button>
+                  </div>
+                </div>
+              </div>
+            </li>
+    `
+  })
+}
+
+const formatDong = (str) => {
+  const co = str.toString().split("");
+  let i = 0;
+  let output = "";
+  for (let j = 0; j < co.length; j++) {
+    output += co[j];
+    ++i;
+    if (i % 3 === 0 && j !== co.length - 1) output += ",";
+  }
+  return output;
+}
+
+const collapseFilterButton = () => {
+  const collapseButtons = document.querySelectorAll('.filter-by-fields-collapse-btn');
+
+  collapseButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      const filterItem = this.nextElementSibling;
+      filterItem.classList.toggle('open');
+    });
+  });
+
+
+
+}
+

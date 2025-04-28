676d3228e92670a497990060684eff3ed9afe829
luan <vanluandvlp@gmail.com>
2025-04-28 20:02:29 +0700

    trang 3

---
+++ chanvay/script.js
@@ -0,0 +1,314 @@
+const data = [
+  {
+    "name": "Chân váy dáng A phối ly thanh lịch",
+    "price": 295000,
+    "color": "Ghi nhạt",
+    "size": "M",
+    "sold": 48,
+    "date": "12/06/2024",
+    "img": "images/chanvay1.jpg"
+  },
+  {
+    "name": "Chân váy bút chì cạp liền đính cúc thanh lịch",
+    "price": 330000,
+    "color": "Đen",
+    "size": "S",
+    "sold": 53,
+    "date": "15/06/2024",
+    "img": "images/chanvay2.jpg"
+  },
+  {
+    "name": "Chân váy xếp ly dáng dài nhẹ nhàng",
+    "price": 390000,
+    "color": "Be",
+    "size": "L",
+    "sold": 27,
+    "date": "17/06/2024",
+    "img": "images/chanvay3.jpg"
+  },
+  {
+    "name": "Chân váy denim dáng A cá tính",
+    "price": 420000,
+    "color": "Xanh navy",
+    "size": "M",
+    "sold": 31,
+    "date": "18/06/2024",
+    "img": "images/chanvay4.jpg"
+  },
+  {
+    "name": "Chân váy midi xòe họa tiết hoa nhí",
+    "price": 450000,
+    "color": "Hồng pastel",
+    "size": "S",
+    "sold": 45,
+    "date": "19/06/2024",
+    "img": "images/chanvay5.jpg"
+  },
+  {
+    "name": "Chân váy xòe xếp tầng dễ thương",
+    "price": 370000,
+    "color": "Trắng kem",
+    "size": "M",
+    "sold": 52,
+    "date": "20/06/2024",
+    "img": "images/chanvay6.jpg"
+  },
+  {
+    "name": "Chân váy bút chì xẻ tà quyến rũ",
+    "price": 410000,
+    "color": "Đỏ đô",
+    "size": "L",
+    "sold": 24,
+    "date": "22/06/2024",
+    "img": "images/chanvay7.jpg"
+  },
+  {
+    "name": "Chân váy mini giả da cá tính",
+    "price": 460000,
+    "color": "Đen",
+    "size": "S",
+    "sold": 38,
+    "date": "23/06/2024",
+    "img": "images/chanvay8.jpg"
+  },
+  {
+    "name": "Chân váy voan xòe 2 lớp nhẹ nhàng",
+    "price": 320000,
+    "color": "Trắng",
+    "size": "M",
+    "sold": 41,
+    "date": "24/06/2024",
+    "img": "images/chanvay9.jpg"
+  },
+  {
+    "name": "Chân váy caro dáng A basic",
+    "price": 360000,
+    "color": "Kẻ caro nâu",
+    "size": "L",
+    "sold": 29,
+    "date": "25/06/2024",
+    "img": "images/chanvay10.jpg"
+  },
+  {
+    "name": "Chân váy kaki xòe năng động",
+    "price": 340000,
+    "color": "Xanh rêu",
+    "size": "S",
+    "sold": 33,
+    "date": "26/06/2024",
+    "img": "images/chanvay11.jpg"
+  },
+  {
+    "name": "Chân váy midi đen trơn đơn giản",
+    "price": 300000,
+    "color": "Đen",
+    "size": "M",
+    "sold": 54,
+    "date": "27/06/2024",
+    "img": "images/chanvay12.jpg"
+  },
+  {
+    "name": "Chân váy dạ tweed kẻ caro sang trọng",
+    "price": 480000,
+    "color": "Ghi đậm",
+    "size": "L",
+    "sold": 22,
+    "date": "28/06/2024",
+    "img": "images/chanvay13.jpg"
+  },
+  {
+    "name": "Chân váy ren ôm body gợi cảm",
+    "price": 430000,
+    "color": "Kem",
+    "size": "S",
+    "sold": 39,
+    "date": "29/06/2024",
+    "img": "images/chanvay14.jpg"
+  },
+  {
+    "name": "Chân váy xếp ly phối màu pastel",
+    "price": 350000,
+    "color": "Xanh mint",
+    "size": "M",
+    "sold": 44,
+    "date": "30/06/2024",
+    "img": "images/chanvay15.jpg"
+  },
+  {
+    "name": "Chân váy kaki dáng chữ A trẻ trung",
+    "price": 310000,
+    "color": "Be",
+    "size": "L",
+    "sold": 26,
+    "date": "01/07/2024",
+    "img": "images/chanvay16.jpg"
+  },
+  {
+    "name": "Chân váy maxi voan in hoa dịu dàng",
+    "price": 470000,
+    "color": "Trắng họa tiết",
+    "size": "S",
+    "sold": 36,
+    "date": "02/07/2024",
+    "img": "images/chanvay17.jpg"
+  },
+  {
+    "name": "Chân váy mini nhung mềm ấm áp",
+    "price": 380000,
+    "color": "Nâu tây",
+    "size": "M",
+    "sold": 31,
+    "date": "03/07/2024",
+    "img": "images/chanvay18.jpg"
+  },
+  {
+    "name": "Chân váy xòe xếp tầng pastel ngọt ngào",
+    "price": 400000,
+    "color": "Hồng phấn",
+    "size": "L",
+    "sold": 29,
+    "date": "04/07/2024",
+    "img": "images/chanvay19.jpg"
+  },
+  {
+    "name": "Chân váy kaki form suông cá tính",
+    "price": 390000,
+    "color": "Xám chì",
+    "size": "S",
+    "sold": 35,
+    "date": "05/07/2024",
+    "img": "images/chanvay20.jpg"
+  },
+  {
+    "name": "Chân váy midi voan phối bèo duyên dáng",
+    "price": 410000,
+    "color": "Kem sữa",
+    "size": "M",
+    "sold": 42,
+    "date": "06/07/2024",
+    "img": "images/chanvay21.jpg"
+  },
+  {
+    "name": "Chân váy maxi xẻ tà quyến rũ",
+    "price": 470000,
+    "color": "Đen bóng",
+    "size": "L",
+    "sold": 20,
+    "date": "07/07/2024",
+    "img": "images/chanvay22.jpg"
+  },
+  {
+    "name": "Chân váy ôm xẻ trước cực tôn dáng",
+    "price": 350000,
+    "color": "Xanh coban",
+    "size": "S",
+    "sold": 47,
+    "date": "08/07/2024",
+    "img": "images/chanvay23.jpg"
+  },
+  {
+    "name": "Chân váy midi linen phối cúc mộc mạc",
+    "price": 430000,
+    "color": "Vàng mơ",
+    "size": "M",
+    "sold": 30,
+    "date": "09/07/2024",
+    "img": "images/chanvay24.jpg"
+  }
+];
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderProduct();
+  collapseFilterButton();
+})
+const renderProduct = () => {
+  const ulProduct = document.querySelector(".content-product-mid ul");
+  ulProduct.innerHTML = "";
+  data.forEach(item => {
+    ulProduct.innerHTML += `
+       <li>
+              <div class="">
+                <div class="spoiler-img">
+                  <a href="#" class="product-preview" style="display: block;overflow: hidden;">
+                    <img src="${item.img}" class="product-preview-img">
+                  </a>
+                  <div class="sub">
+                    <a href="#">Xem chi tiết</a>
+                    <a href="#">Mua ngay</a>
+                  </div>
+                </div>
+                <div class="spoiler-details">
+                  <div class="item-name">
+                    <a href="#" class="tp_product_name overflow-text" style="display: block;">
+                      <span>${item.name}</span>
+                    </a>
+                  </div>
+                  <div class="item-rate">
+                    <i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path
+                          d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                      </svg></i>
+                    <i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path
+                          d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                      </svg></i>
+                    <i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path
+                          d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                      </svg></i>
+                    <i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path
+                          d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                      </svg></i>
+                    <i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path
+                          d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                      </svg></i>
+                    <span>(${item.sold} đã bán)</span>
+                  </div>
+
+                  <div class="item-prices">
+                    <span>${formatDong(item.price)}<span>₫</span></span>
+                  </div>
+                  <div class="item-btn mrg-top-5">
+                    <button class="item-addbtn">Thêm vào giỏ hàng</button>
+                  </div>
+                </div>
+              </div>
+            </li>
+    `
+  })
+}
+
+const formatDong = (str) => {
+  const co = str.toString().split("");
+  let i = 0;
+  let output = "";
+  for (let j = 0; j < co.length; j++) {
+    output += co[j];
+    ++i;
+    if (i % 3 === 0 && j !== co.length - 1) output += ",";
+  }
+  return output;
+}
+
+const collapseFilterButton = () => {
+  const collapseButtons = document.querySelectorAll('.filter-by-fields-collapse-btn');
+
+  collapseButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      const filterItem = this.nextElementSibling;
+      filterItem.classList.toggle('open');
+    });
+  });
+
+
+
+}
+
